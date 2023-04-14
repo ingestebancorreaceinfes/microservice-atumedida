e93b029ad30dbf3129ec8f3944ed546fc4a3107d
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2023-04-14 09:04:05 -0500

    Cambiando uuid en document-types y grades

--- src/student/data/documentTypes.ts
+++ src/student/data/documentTypes.ts
@@ -1,42 +1,42 @@
 
 export const documentTypes = [
     {
-        "id": "e2f3b210-f993-4dbe-bc03-2d140d969cf8",
+        "id": 1,
         "name": "Cédula Ciudadanía",
         "alias": "CC"
     },
     {
-        "id": "611b9586-5e26-4b26-bf3f-e1c0cfd5efcc",
+        "id": 2,
         "name": "NIT",
         "alias": "NIT"
     },
     {
-        "id": "4acaaa9a-b25d-4d9b-8c90-db5d8936d253",
+        "id": 3,
         "name": "Tarjeta Identidad",
         "alias": "TI"
     },
     {
-        "id": "ad61176a-bd7f-4c19-a11e-f36777a4fb24",
+        "id": 4,
         "name": "Cédula Extranjería",
         "alias": "CEXT"
     },
     {
-        "id": "14a4c587-0f82-4e15-92dc-b0c043fa6751",
+        "id": 5,
         "name": "Pasaporte",
         "alias": "PASS"
     },
     {
-        "id": "b37d8f0d-dd1b-4352-97ee-cd79780c4905",
+        "id": 6,
         "name": "Registro Civil",
         "alias": "RC"
     },
     {
-        "id": "6e9527eb-cc2e-46fd-8233-91a12ac01925",
+        "id": 7,
         "name": "NUIP",
         "alias": "NUIP"
     },
     {
-        "id": "d6cf3f4f-d453-42be-8708-d8932322bb75",
+        "id": 8,
         "name": "Permiso Especial de Permanencia",
         "alias": "PEP"
     }
