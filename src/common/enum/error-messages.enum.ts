c9734ea44e2a87d5926f9b08dd5d2c3f4b684d5f
Esteban Correa <estebancorrea405@gmail.com>
2023-04-17 09:47:32 -0500

    Modificando nombre de variable application_error

--- src/common/enum/error-messages.enum.ts
+++ src/common/enum/error-messages.enum.ts
@@ -1,5 +1,5 @@
 export enum ErrorMessages {
-    APPLICATION_ERROR = 'Error de aplicación',
+    APPLICATION_ERROR = 'Application error',
     BAD_REQUEST = '',
     CREATED = 'Created Succesfully',
     OK_RESPONSE = 'The resource was returned successfully',
