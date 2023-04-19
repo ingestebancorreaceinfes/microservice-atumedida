ae54589cab0b782cbf2a9512c6c576f42ef5ea83
Esteban Correa <estebancorrea405@gmail.com>
2023-04-19 12:13:44 -0500

    Comentando registro de datos de usuario

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -70,7 +70,7 @@ export class StudentService {
                 const arrayFullName = name.split(" ", 3);
                 newStudent.name = arrayFullName[0];
                 newStudent.lastname = arrayFullName[1]+" "+arrayFullName[2];
-                this.studentRepository.save(newStudent);
+                // this.studentRepository.save(newStudent);
                 return HttpStatus.CREATED;
             }else{  
                 throw new BadRequestException(ErrorMessages.BAD_REQUEST);
