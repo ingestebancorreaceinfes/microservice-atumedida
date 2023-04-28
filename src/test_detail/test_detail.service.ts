0a92c1024d3990fe1284d16682a382d4a6776075
Esteban Correa <estebancorrea405@gmail.com>
2023-04-28 09:59:58 -0500

    Actualizando lógica de StudentTest

--- src/test_detail/test_detail.service.ts
+++ src/test_detail/test_detail.service.ts
@@ -21,7 +21,20 @@ export class TestDetailService {
         return query;
     }
 
-    getResults(id: string) {
-        throw new Error('Method not implemented.');
+    async getResults(testId: string) {
+        const query = await this.testDetailRepository.query(
+            `select td."order" "order",td.question_id,q.answer,
+            (select c.name from competences c where c.id = q.competence_id) competence,
+            (select co.name from components co where co.id = q.component_id) component,
+            (select tas.name from tasks tas where tas.id = q.task_id) task,
+            (select a.name from areas a where a.id = td.area_id) area,
+            (select s.name from subjects s where s.id = td.subject_id) subject
+            from tests_details td
+            inner join questions q on q.id = td.question_id
+            where td.test_id = '${testId}'
+            order by td.order;`
+        );
+
+        return query;
     }
 }
