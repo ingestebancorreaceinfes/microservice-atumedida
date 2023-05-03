9c6a5585ee48221ac35e652bd1fb9b0050949a89
Esteban Correa <estebancorrea405@gmail.com>
2023-05-03 17:32:43 -0500

    Quitando ruta /api/v1/test/{id}/score en test_controller

--- src/test_detail/test_detail.controller.ts
+++ src/test_detail/test_detail.controller.ts
@@ -1,25 +1,9 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { TestDetailService } from './test_detail.service';
-import { ApiHeader, ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ErrorMessages } from 'src/common/enum/error-messages.enum';
-import { AuthnGuard } from 'src/auth/guards/auth.guard';
-import { Headers } from '@nestjs/common';
-import { SuccessMessages } from 'src/common/enum/success-messages.enum';
 
-@ApiTags('Test Details')
 @Controller('')
 export class TestDetailController {
   constructor(private readonly testDetailService: TestDetailService) {}
 
-  @ApiOkResponse({ description: SuccessMessages.OK_RESPONSE })
-  @ApiResponse({ status: 401, description: ErrorMessages.NOT_VALID_TOKEN })
-  @ApiResponse({ status: 403, description: ErrorMessages.FORBIDDEN })
-  @ApiResponse({status:500, description: ErrorMessages.APPLICATION_ERROR})
-  @ApiHeader({name: 'Authorization',description: 'Generated token by authentication microservice',required: true})
-  @UseGuards(AuthnGuard) 
-  @Get('test/:id/score')
-  getResults(@Param('id') id: string){
-    this.testDetailService.getResults(id);
-  }
 
 }
