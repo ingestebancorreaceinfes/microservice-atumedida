f240278f6fcfb5ff0c7c3909675ee40200a7235e
Esteban Correa <estebancorrea405@gmail.com>
2023-05-11 15:55:08 -0500

    Comentando function getDocumentTypes()

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -19,7 +19,7 @@ export class StudentService {
     ){}
     
     getDocumentTypes(): string {
-        return JSON.stringify(documentTypes);
+        return JSON.stringify(documentTypes);//Convierte un Object JS a JSON
     }
     
     getStates(): Promise<States[]> {
